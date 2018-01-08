87283ec921bc7b89e766131365a4c75157785da7
mang <ningma003@gmail.com>
2018-01-08 11:25:24 +0800

    * 修改配置文件

--- src/main/java/mang/tools/ftp/download/FTPDownloadSkeleton.java
+++ src/main/java/mang/tools/ftp/download/FTPDownloadSkeleton.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 /**
